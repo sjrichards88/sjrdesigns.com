0a23b5bbf30897b314ccd8a2ac934625e73c9906
Simon Richards <s.richards@dewynters.com>
2024-06-23 18:59:37 +0100

    Update email.ts

--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -1,25 +1,29 @@
 import { Resend } from 'resend';
 
-export async function POST({ request, context }) {
-  const resend = new Resend(context.env.RESEND_API_KEY);
-  const body = await request.json();
-  const { name, email, message } = body;
+export async function POST(context) {
 
-  const { data, error } = await resend.emails.send({
-    from: 'SJR Designs <info@email.sjrdesigns.com>',
-    to: 'sjrichards88@gmail.com',
-    subject: 'sjrdesigns.com Contact Form Submission',
-    html: `
-      <p>Name: ${name}</p>
-      <p>Email: ${email}</p>
-      <p>Message: ${message}</p>
-    `,
-  });
+  console.log(context)
+
+  // const resend = new Resend(context.env.RESEND_API_KEY);
+  // const body = await request.json();
+  // const { name, email, message } = body;
+
+  // const { data, error } = await resend.emails.send({
+  //   from: 'SJR Designs <info@email.sjrdesigns.com>',
+  //   to: 'sjrichards88@gmail.com',
+  //   subject: 'sjrdesigns.com Contact Form Submission',
+  //   html: `
+  //     <p>Name: ${name}</p>
+  //     <p>Email: ${email}</p>
+  //     <p>Message: ${message}</p>
+  //   `,
+  // });
 
   return new Response(
-    JSON.stringify({
-      data,
-      error
-    })
+    // JSON.stringify({
+    //   data,
+    //   error
+    // })
+  "helloworld"
   )
 }
