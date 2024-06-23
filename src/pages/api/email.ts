ef37aecca0745bc871e41be05cb588ec25644d00
Simon Richards <s.richards@dewynters.com>
2024-06-23 13:41:09 +0100

    Update email.ts

--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -1,25 +1,27 @@
-import { Resend } from 'resend';
+// import { Resend } from 'resend';
 
 export async function POST({ request }) {
-  const resend = new Resend(import.meta.env.RESEND_API_KEY);
+  // const resend = new Resend(import.meta.env.RESEND_API_KEY);
   const body = await request.json();
   const { name, email, message } = body;
 
-  const { data, error } = await resend.emails.send({
-    from: 'SJR Designs <info@email.sjrdesigns.com>',
-    to: 'sjrichards88@gmail.com',
-    subject: 'sjrdesigns.com Contact Form Submission',
-    html: `
-      <p>Name: ${name}</p>
-      <p>Email: ${email}</p>
-      <p>Message: ${message}</p>
-    `,
-  });
+  // const { data, error } = await resend.emails.send({
+  //   from: 'SJR Designs <info@email.sjrdesigns.com>',
+  //   to: 'sjrichards88@gmail.com',
+  //   subject: 'sjrdesigns.com Contact Form Submission',
+  //   html: `
+  //     <p>Name: ${name}</p>
+  //     <p>Email: ${email}</p>
+  //     <p>Message: ${message}</p>
+  //   `,
+  // });
 
   return new Response(
     JSON.stringify({
-      data,
-      error
+        name,
+        email,
+        message,
+      // error
     })
   )
 }
